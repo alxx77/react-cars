90409e93439bc6d96211a0b1e992ef3669bffbf4
Aleksandar Srdic <srle01@gmail.com>
2021-01-04 11:06:16 +0100

    aaa

--- src/App.js
+++ src/App.js
@@ -2,20 +2,19 @@ import React, { useEffect } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Main from "./components/Main";
-import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from "./Login";
 import UpsertItem from "./UpsertItem";
-import { setData} from "./components/actions";
+import { setData } from "./components/actions";
 import axios from "axios";
 import store from "./components/Store";
 
 function App() {
-
-  
-
   useEffect(() => {
     (async () => {
-      const result = await axios("http://localhost:3003/api/get_all_items",{withCredentials:true});
+      const result = await axios("http://localhost:3003/api/get_all_items", {
+        withCredentials: true,
+      });
 
       store.dispatch(setData(result.data));
     })();
@@ -32,18 +31,17 @@ function App() {
           <Route path="/signin">
             <Login />
           </Route>
-          <Route path="/settings">
-          </Route>
+          <Route path="/settings"></Route>
           <Route
             exact
             path="/edit_item/:id_pon"
             //render={(props) => <UpsertItem {...props}/>}
-            render={()=><UpsertItem/>}
+            render={() => <UpsertItem />}
           ></Route>
           <Route
             exact
             path="/insert_item/:id_pon"
-            render={() => <UpsertItem/>}
+            render={() => <UpsertItem />}
           />
         </Switch>
       </div>
